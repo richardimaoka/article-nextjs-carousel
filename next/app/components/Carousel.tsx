8baf422e73d534edfbbff2308df01c5861a94d9c
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-02-19 05:02:17 +0900

    CarouselItem component

---
+++ next/app/components/Carousel.tsx
@@ -0,0 +1,23 @@
+import styles from "./Carousel.module.css";
+import { CarouselItem, CarouselItemProps } from "./CarouselItem";
+
+interface Props {
+  items: CarouselItemProps[];
+}
+
+export function Carousel(props: Props) {
+  return (
+    <div className={styles.component}>
+      <div>
+        {props.items.map((x) => (
+          <CarouselItem
+            key={x.src}
+            width={x.width}
+            height={x.height}
+            src={x.src}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ next/app/components/CarouselItem.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+import styles from "./CarouselItem.module.css";
+import Image from "next/image";
+
+interface Props {
+  width: number;
+  height: number;
+  src: string;
+}
+
+export type CarouselItemProps = Props;
+
+export function CarouselItem(props: Props) {
+  return (
+    <div className={styles.component}>
+      <Image
+        width={props.width}
+        height={props.height}
+        src={props.src}
+        alt={props.src}
+      />
+    </div>
+  );
+}
